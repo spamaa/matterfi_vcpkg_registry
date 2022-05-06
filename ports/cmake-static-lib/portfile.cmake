6f2bce45e96a9519cd030deb1e2dd5ef5b976487
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-05-06 12:34:31 +0200

    cmake-static-lib update

--- ports/cmake-static-lib/portfile.cmake
+++ ports/cmake-static-lib/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
   OUT_SOURCE_PATH SOURCE_PATH
   URL git-matterfi@odroidH2:repos/cmake-static-lib.git
-  REF 7ebd700a99565b123cc70b6c4332a5f958925765
+  REF c805c5fd44e7091a8caa02217a16d1a3d86a20ff
   FETCH_REF master
   HEAD_REF master
 )
