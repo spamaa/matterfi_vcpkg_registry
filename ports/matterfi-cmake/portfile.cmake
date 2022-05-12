23c908dfe2af39023b52a7492e727c047bcc32ae
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-05-12 14:21:48 +0200

    fixes in matterfi-cmake

--- ports/matterfi-cmake/portfile.cmake
+++ ports/matterfi-cmake/portfile.cmake
@@ -1,10 +1,14 @@
 #include("${CMAKE_CURRENT_LIST_DIR}/../vcpkg-cmake/vcpkg-port-config.cmake")
 
+message (STATUS "!!!!** BEGIN matterf-cmake BEGIN **!!!!")
 message (STATUS "!!!!** _HOST_TRIPLET: '${_HOST_TRIPLET}' **!!!!")
+message (STATUS "!!!!** CMAKE_CURRENT_LIST_DIR: '${CMAKE_CURRENT_LIST_DIR}' **!!!!")
 
 file (INSTALL 
-	"{CMAKE_CURRENT_LIST_DIR}/matterfi_config.cmake"
+	"${CMAKE_CURRENT_LIST_DIR}/matterfi_config.cmake"
 	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 )
 
 set (VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+message (STATUS "!!!!** END matterf-cmake END **!!!!")
