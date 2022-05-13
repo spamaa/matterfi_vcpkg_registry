139a01da5c1172fdea6f3a29a8d36f520ce074a8
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-05-13 12:34:59 +0200

    add nested cmake module in matterfi-cmake

--- ports/matterfi-cmake/matterfi_config.cmake
+++ ports/matterfi-cmake/matterfi_config.cmake
@@ -1,8 +1,1 @@
-# this is how to include another cmakes
-# include("${CMAKE_CURRENT_LIST_DIR}/vcpkg_cmake_build.cmake")
-
-function (matterfi_hello)
-	message (STATUS "!!!! WOW WOW WOW WOW !!!!")
-	message (STATUS "!!!! WOW CMAKE_CURRENT_LIST_DIR: '${CMAKE_CURRENT_LIST_DIR} WOW !!!!'")
-	message (STATUS "!!!! WOW WOW WOW WOW !!!!")
-endfunction()
+include("${CMAKE_CURRENT_LIST_DIR}/matterfi_hello.cmake")
---
+++ ports/matterfi-cmake/matterfi_hello.cmake
@@ -0,0 +1,6 @@
+# this is test module
+function (matterfi_hello)
+	message (STATUS "!!!! WOW WOW WOW WOW !!!!")
+	message (STATUS "!!!! WOW CMAKE_CURRENT_LIST_DIR: '${CMAKE_CURRENT_LIST_DIR} WOW !!!!'")
+	message (STATUS "!!!! WOW WOW WOW WOW !!!!")
+endfunction()
