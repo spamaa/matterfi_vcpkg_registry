d18ea29d7a066344d18f5c60f5e975bd4cfa4931
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-05-13 13:05:24 +0200

    fix in matterfi-cmake

--- ports/matterfi-cmake/matterfi_config.cmake
+++ ports/matterfi-cmake/matterfi_config.cmake
@@ -1,3 +1,1 @@
-message (STATUS "!!!! WOS CMAKE_CURRENT_LIST_DIR: '${CMAKE_CURRENT_LIST_DIR}' WOS !!!!'")
-message (STATUS "!!!! WOS CMAKE_CURRENT_SOURCE_DIR  : '${CMAKE_CURRENT_SOURCE_DIR}' WOS !!!!'")
 include("${CMAKE_CURRENT_LIST_DIR}/matterfi_hello.cmake")
--- ports/matterfi-cmake/portfile.cmake
+++ ports/matterfi-cmake/portfile.cmake
@@ -1,22 +1,8 @@
-#include("${CMAKE_CURRENT_LIST_DIR}/../vcpkg-cmake/vcpkg-port-config.cmake")
-
-message (STATUS "!!!!** BEGIN matterf-cmake BEGIN **!!!!")
-message (STATUS "!!!!** _HOST_TRIPLET: '${_HOST_TRIPLET}' **!!!!")
-message (STATUS "!!!!** CMAKE_CURRENT_LIST_DIR: '${CMAKE_CURRENT_LIST_DIR}' **!!!!")
-message (STATUS "!!!!** CURRENT_PACKAGES_DIR: '${CURRENT_PACKAGES_DIR}' **!!!!")
-message (STATUS "!!!!** CURRENT_PORT_DIR: '${CURRENT_PORT_DIR}' **!!!!")
-message (STATUS "!!!!** CURRENT_INSTALLED_DIR: '${CURRENT_INSTALLED_DIR}' **!!!!")
-message (STATUS "!!!!** PORT: '${PORT}' **!!!!")
-message (STATUS "!!!!** PACKAGE_NAME: '${PACKAGE_NAME}' **!!!!")
-message (STATUS "!!!!** _PACKAGE_NAME: '${_PACKAGE_NAME}' **!!!!")
-message (STATUS "!!!!** _VCPKG_INSTALLED_DIR: '${_VCPKG_INSTALLED_DIR}' **!!!!")
-message (STATUS "!!!!** BEGIN matterf-cmake BEGIN **!!!!")
-
 file (INSTALL 
 	"${CMAKE_CURRENT_LIST_DIR}/matterfi_config.cmake"
+	"${CMAKE_CURRENT_LIST_DIR}/matterfi_hello.cmake"
 	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 )
 
 set (VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
-message (STATUS "!!!!** END matterf-cmake END **!!!!")
