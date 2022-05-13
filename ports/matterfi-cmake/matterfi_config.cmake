5638ea6127419289320038530173bbfc7bf41060
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-05-13 12:56:42 +0200

    fix in matterfi-cmake

--- ports/matterfi-cmake/matterfi_config.cmake
+++ ports/matterfi-cmake/matterfi_config.cmake
@@ -1,3 +1,3 @@
 message (STATUS "!!!! WOS CMAKE_CURRENT_LIST_DIR: '${CMAKE_CURRENT_LIST_DIR}' WOS !!!!'")
-message (STATUS "!!!! WOS CMAKE_CURRENT_SOURCE_DIR  : '${CMAKE_CURRENT_SOURCE_DIR  }' WOS !!!!'")
+message (STATUS "!!!! WOS CMAKE_CURRENT_SOURCE_DIR  : '${CMAKE_CURRENT_SOURCE_DIR}' WOS !!!!'")
 include("${CMAKE_CURRENT_LIST_DIR}/matterfi_hello.cmake")
