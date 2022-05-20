0dc7399a401fc2a2d8db4fb4e9111ff29275e9ac
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-05-20 16:15:53 +0200

    cmake-static-lib port 2

--- ports/cmake-static-lib/portfile.cmake
+++ ports/cmake-static-lib/portfile.cmake
@@ -1,16 +1,23 @@
 if (MT_USE_LOCAL_REPOSITORY)
-	message(STATUS "!!!! LOCAL REPO: MT_USE_LOCAL_REPOSITORY: '${MT_USE_LOCAL_REPOSITORY}' !!!!")
+	message(STATUS "Fetching sources from github: 'cmake-static-lib'")
+	vcpkg_from_github(
+	  OUT_SOURCE_PATH SOURCE_PATH
+	  REPO spamaa/mt_cmake_static_lib
+	  REF 806cee3c01e0e21cdde729f43054b67ff2c0868c
+	  SHA512 0
+	  HEAD_REF master
+	)	
 else()
-	message(STATUS "!!!! GITHUB REPO: 	MT_USE_LOCAL_REPOSITORY: '${MT_USE_LOCAL_REPOSITORY}' !!!!")
+	message(STATUS "Fetching sources from local repository: 'cmake-static-lib'")
+	vcpkg_from_git(
+	  OUT_SOURCE_PATH SOURCE_PATH
+	  URL git-matterfi@odroidH2:repos/CMakeStaticLib.git
+	  REF 806cee3c01e0e21cdde729f43054b67ff2c0868c
+	  FETCH_REF master
+	  HEAD_REF master
+	)
 endif()
 
-vcpkg_from_git(
-  OUT_SOURCE_PATH SOURCE_PATH
-  URL git-matterfi@odroidH2:repos/CMakeStaticLib.git
-  REF 806cee3c01e0e21cdde729f43054b67ff2c0868c
-  FETCH_REF master
-  HEAD_REF master
-)
 
 vcpkg_configure_cmake(
   SOURCE_PATH "${SOURCE_PATH}"
